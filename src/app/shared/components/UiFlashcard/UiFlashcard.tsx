b0c9caa8e535f07f8ed932ae515cb300352c0a28
Marina Ismailova <marina1988sherbakova@gmail.com>
2022-03-09 03:34:36 -0500

    main: fix styles for flashcards

--- src/app/shared/components/UiFlashcard/UiFlashcard.tsx
+++ src/app/shared/components/UiFlashcard/UiFlashcard.tsx
@@ -11,11 +11,11 @@ export interface UiFlashcardProps {
 export const UiFlashcard: React.FC<UiFlashcardProps> = ({title, active, children}) => {
   return (
     <div className="ui-flashcard">
-      <div className="ui-flashcard__header">
-        {title && (<h4 className="ui-flashcard__title">{title}</h4>)}
-        <span>{active ? <FaEye/> : <FaEyeSlash/>}</span>
-      </div>
       <div className="ui-flashcard__body">
+        <div className="ui-flashcard__header">
+          {title && (<h4 className="ui-flashcard__title">{title}</h4>)}
+          <span className="ui-flashcard__icon">{active ? <FaEye/> : <FaEyeSlash/>}</span>
+        </div>
         {children}
       </div>
     </div>
